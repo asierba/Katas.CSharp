5f9061742991b4813bb873d9a9c704fd80d72460
Asier Barrenetxea <asier.barrenetxea-arando@wiggle.co.uk>
2013-08-02 15:12:13 +0100

    Bowling Game kata added

---
+++ BowlingGame/GameFixture.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+
+namespace BowlingGame
+{
+    [TestFixture]
+    public class GameFixture
+    {
+        private Game _g;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _g = new Game();
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+                _g.Roll(pins);
+        }
+
+        [Test]
+        public void TestGutterGame()
+        {
+            RollMany(20, 0);
+            Assert.AreEqual(0, _g.Score);
+        }
+
+        [Test]
+        public void TestAllOnes()
+        {
+            RollMany(20, 1);
+            Assert.AreEqual(20, _g.Score);
+        }
+
+        [Test]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            _g.Roll(3);
+            RollMany(17, 0);
+            Assert.AreEqual(16, _g.Score);
+        }
+
+        [Test]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            _g.Roll(3);
+            _g.Roll(4);
+            RollMany(16,0);
+            Assert.AreEqual(24,_g.Score);
+        }
+
+        [Test]
+        public void TestPerfectGame()
+        {
+            RollMany(12,10);
+            Assert.AreEqual(300, _g.Score);
+        }
+
+        private void RollSpare()
+        {
+            _g.Roll(5);
+            _g.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            _g.Roll(10);
+        }
+    }
+}
--- Readme.md
+++ Readme.md
@@ -65,3 +65,24 @@ String Calculator
 Calculates the sum of integers in a string separated by a comma.
 
 Kata obtained from http://osherove.com/tdd-kata-1/
+
+Bowling game
+------------
+"The game consists of 10 frames as shown above.  In each frame the player has
+two opportunities to knock down 10 pins.  The score for the frame is the total
+number of pins knocked down, plus bonuses for strikes and spares.
+
+A spare is when the player knocks down all 10 pins in two tries.  The bonus for
+that frame is the number of pins knocked down by the next roll.  So in frame 3
+above, the score is 10 (the total number knocked down) plus a bonus of 5 (the
+number of pins knocked down on the next roll.)
+
+A strike is when the player knocks down all 10 pins on his first try.  The bonus
+for that frame is the value of the next two balls rolled.
+
+In the tenth frame a player who rolls a spare or strike is allowed to roll the extra
+balls to complete the frame.  However no more than three balls can be rolled in
+tenth frame."
+
+Kata obtained from http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
+From the book Agile Principles, Patterns, and Practices by Rorbert C. Martin
