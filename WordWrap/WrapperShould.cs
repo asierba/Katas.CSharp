e94acf40a0ac461084b754c88d34e9a5d62fd841
asierba <1537266+asierba@users.noreply.github.com>
2015-02-24 22:05:36 +0000

    More refactorings

--- WordWrap/WrapperShould.cs
+++ WordWrap/WrapperShould.cs
@@ -45,30 +45,22 @@ namespace WordWrap
     {
         public static string Wrap(string text, int columns)
         {
-            if (text.Length > columns)
-            {
-                if (text[columns] == ' ')
-                    return Split(text, columns, columns, 1); 
-                var previousSpace = text.Substring(0, columns).LastIndexOf(' ');
-                if (previousSpace != -1)
-                {
-                    return Split(text, columns, previousSpace, 1);
-                }
+            if (text.Length <= columns) 
+                return text;
 
-                return Split(text, columns);
-            }
-                
-            return text;
+            if (text[columns] == ' ')
+                return Split(text, columns, 1); 
+
+            var previousSpace = text.Substring(0, columns).LastIndexOf(' ');
+            if (previousSpace != -1)
+                return Split(text, previousSpace, 1);
+
+            return Split(text, columns);
         }
 
-        private static string Split(string text, int position)
+        private static string Split(string text, int position, int offset = 0)
         {
-            return Split(text, position, position);
-        }
-
-        private static string Split(string text, int position, int newPosition, int offset = 0)
-        {
-            return text.Substring(0, newPosition) + '\n' + Wrap(text.Substring(newPosition + offset), position);
+            return text.Substring(0, position) + '\n' + Wrap(text.Substring(position + offset), position);
         }
     }
 }
