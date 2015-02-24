ec06b0095a55f23ee2b46ef67b8f96ec845641f2
asierba <1537266+asierba@users.noreply.github.com>
2015-02-24 20:09:36 +0000

    Emtpy string

--- WordWrap/WrapperTests.cs
+++ WordWrap/WrapperTests.cs
@@ -5,8 +5,17 @@ namespace WordWrap
     public class WrapperTests
     {
         [Test]
-        public void Test()
+        public void EmtpyString()
         {
+            Assert.AreEqual("", Wrapper.Wrap("", 0));
+        }
+    }
+
+    public class Wrapper
+    {
+        public static string Wrap(string text, int columns)
+        {
+            return "";
         }
     }
 }
