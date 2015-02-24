5bbe6f1c27b63d40e92286233c61a09f88fbf214
asierba <1537266+asierba@users.noreply.github.com>
2015-02-24 22:08:05 +0000

    Wrapper Extracted to a different file

---
+++ WordWrap/Wrapper.cs
@@ -0,0 +1,25 @@
+﻿namespace WordWrap
+{
+    public class Wrapper
+    {
+        public static string Wrap(string text, int columns)
+        {
+            if (text.Length <= columns) 
+                return text;
+
+            if (text[columns] == ' ')
+                return Split(text, columns, 1); 
+
+            var previousSpace = text.Substring(0, columns).LastIndexOf(' ');
+            if (previousSpace != -1)
+                return Split(text, previousSpace, 1);
+
+            return Split(text, columns);
+        }
+
+        private static string Split(string text, int position, int offset = 0)
+        {
+            return text.Substring(0, position) + '\n' + Wrap(text.Substring(position + offset), position);
+        }
+    }
+}
--- WordWrap/WrapperShould.cs
+++ WordWrap/WrapperShould.cs
@@ -40,27 +40,4 @@ namespace WordWrap
             Assert.AreEqual(" two\ntwo\ntwo", Wrapper.Wrap(" two two two", 4));
         }
     }
-
-    public class Wrapper
-    {
-        public static string Wrap(string text, int columns)
-        {
-            if (text.Length <= columns) 
-                return text;
-
-            if (text[columns] == ' ')
-                return Split(text, columns, 1); 
-
-            var previousSpace = text.Substring(0, columns).LastIndexOf(' ');
-            if (previousSpace != -1)
-                return Split(text, previousSpace, 1);
-
-            return Split(text, columns);
-        }
-
-        private static string Split(string text, int position, int offset = 0)
-        {
-            return text.Substring(0, position) + '\n' + Wrap(text.Substring(position + offset), position);
-        }
-    }
 }
